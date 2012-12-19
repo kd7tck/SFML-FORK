d0072498f6485d3006c6c71ab3746ffca7705b06
kd7tck <kd7tck@kd7tck-N68S3.(none)>
2012-12-19 09:42:16 -0800

    fix for merge conflict.

--- include/SFML/Graphics.hpp
+++ include/SFML/Graphics.hpp
@@ -11,12 +11,12 @@
 // subject to the following restrictions:
 //
 // 1. The origin of this software must not be misrepresented;
-//    you must not claim that you wrote the original software.
-//    If you use this software in a product, an acknowledgment
-//    in the product documentation would be appreciated but is not required.
+// you must not claim that you wrote the original software.
+// If you use this software in a product, an acknowledgment
+// in the product documentation would be appreciated but is not required.
 //
 // 2. Altered source versions must be plainly marked as such,
-//    and must not be misrepresented as being the original software.
+// and must not be misrepresented as being the original software.
 //
 // 3. This notice may not be removed or altered from any source distribution.
 //
@@ -50,8 +50,6 @@
 #include <SFML/Graphics/Vertex.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
 #include <SFML/Graphics/View.hpp>
-#include <SFML/Graphics/GifReader.hpp>
-#include <SFML/Graphics/SlideShow.hpp>
 
 
 #endif // SFML_GRAPHICS_HPP
