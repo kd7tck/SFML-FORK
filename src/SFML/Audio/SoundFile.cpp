3efea482ae2d603f13e0078bd6d25946abd0c67b
kd7tck <kd7tck@kd7tck-N68S3.(none)>
2013-03-12 03:42:04 -0700

    fixed soundbuffer loadfromfile duplicate buffer error.

--- src/SFML/Audio/SoundFile.cpp
+++ src/SFML/Audio/SoundFile.cpp
@@ -25,7 +25,7 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <SFML/Audio/SoundFile.hpp>
+#include "SoundFile.hpp"
 #include <SFML/System/InputStream.hpp>
 #include <SFML/System/Err.hpp>
 #include <cstring>
